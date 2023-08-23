9de1a01a6337f55a5c12459386148ada9557fd5f
King-Windows <krsazon@gmail.com>
2023-08-22 23:05:54 -0700

    2D arrays'

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
+("use strict");
 import "./style.scss";
 
 document.querySelector("#app").innerHTML = `
   <div>
     <h1>Tic Tac Toe</h1>
+    <div id="target"></div>
   </div>
 `;
-
-("use strict");
-
+// Initiates Game Board
 const gameBoard = (() => {
   console.log("initiates gameBoard");
-  let gameBoard = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+  const rows = 3;
+  const cols = 3;
+  const board = [];
+
+  // Adds 2d array
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < cols; j++) {
+      board[i].push("X");
+      console.log(board[i]);
+    }
+  }
+})();
+
+const Cell = (() => {
+  console.log("initiates Cell");
 })();
 
 const displayController = (() => {
@@ -20,5 +35,3 @@ const displayController = (() => {
 const player = () => {
   console.log("initiates player");
 };
-
-
