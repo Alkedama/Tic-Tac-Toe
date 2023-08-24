1b867d6ed3246b47e64333eb1a8f8496836a1464
King-Windows <krsazon@gmail.com>
2023-08-23 21:30:58 -0700

    UI (#3)

--- main.js
+++ main.js
@@ -4,9 +4,24 @@ import "./style.scss";
 document.querySelector("#app").innerHTML = `
   <div>
     <h1>Tic Tac Toe</h1>
+    <div class="board">
+    <div class="cell">X</div>
+    <div class="cell">O</div>
+    <div class="cell">X</div>
+    <div class="cell">O</div>
+    <div class="cell">X</div>
+    <div class="cell">O</div>
+    <div class="cell">X</div>
+    <div class="cell">O</div>
+    <div class="cell">X</div>
+    </div>
     <div id="target"></div>
   </div>
 `;
+
+
+
+
 // Initiates Game Board
 const gameBoard = (() => {
   console.log("initiates gameBoard");
@@ -18,15 +33,36 @@ const gameBoard = (() => {
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < cols; j++) {
-      board[i].push("X");
+      board[i].push(Cell());
       console.log(board[i]);
     }
   }
-})();
 
-const Cell = (() => {
+  const cell = document.querySelectorAll(".cell");
+  let i = 0;
+  cell.forEach(buttons => {
+    const cellButton = document.createElement("button");
+    buttons.appendChild(cellButton);
+
+    cellButton.addEventListener("click", () => {
+      console.log("clicked");
+      cellButton.textContent = "X";
+    })
+    cellButton.textContent = i;
+    i++;
+  });
+
+  
+});
+
+// Represents a cell in the board
+const Cell = () => {
+  let value = "X";
   console.log("initiates Cell");
-})();
+  return { value }
+};
+
+gameBoard();
 
 const displayController = (() => {
   console.log("initiates displayController");
